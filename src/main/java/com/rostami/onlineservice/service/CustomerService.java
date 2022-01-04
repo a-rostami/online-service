91831fc2e4c6f132020fe501f745eabf7c2fe129
arash <arashrostami259@gmail.com>
2022-01-04 11:14:33 +0330

    Add logback-spring.xml for logging.

--- src/main/java/com/rostami/onlineservice/service/CustomerService.java
+++ src/main/java/com/rostami/onlineservice/service/CustomerService.java
@@ -5,6 +5,8 @@ import com.rostami.onlineservice.exception.DuplicateEmailException;
 import com.rostami.onlineservice.repository.CustomerRepository;
 import com.rostami.onlineservice.service.base.BaseService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +17,7 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 public class CustomerService extends BaseService<Customer, Long> {
     private final CustomerRepository repository;
+    Logger logger = LoggerFactory.getLogger(CustomerService.class);
 
     @PostConstruct
     public void init(){
