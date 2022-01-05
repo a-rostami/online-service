c986d0d46b7b64bd64ccc9da5aac2de07b1ff61b
arash <arashrostami259@gmail.com>
2022-01-05 17:05:36 +0330

    Add LoggingAspect.java And Aspect Configurations.

---
+++ src/main/java/com/rostami/onlineservice/aop/LoggingAspect.java
@@ -0,0 +1,39 @@
+package com.rostami.onlineservice.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    //AOP expression for which methods shall be intercepted
+    @Around("execution(* com.rostami.onlineservice.service.*.*(..))")
+    public Object logAllMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+        //Get intercepted method details
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        StopWatch stopWatch = new StopWatch();
+
+        //Measure method execution time
+        stopWatch.start();
+        Object result = joinPoint.proceed();
+        stopWatch.stop();
+
+        //Log method execution time
+        logger.info("Execution time of " + className + "." + methodName + " :: " + stopWatch.getTotalTimeMillis() + " ms");
+
+        return result;
+    }
+
+}
--- src/test/java/com/rostami/onlineservice/config/AppConfig.java
+++ src/test/java/com/rostami/onlineservice/config/AppConfig.java
@@ -2,9 +2,11 @@ package com.rostami.onlineservice.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @TestConfiguration
 @ComponentScan("com.rostami.onlineservice")
+@EnableAspectJAutoProxy
 public class AppConfig {}
 
